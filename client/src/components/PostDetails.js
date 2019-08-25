0fe6f62108653b28b762a9183013434da3460db3
Ryan Andrew Sibat <ryanandrew.sibat@gmail.com>
2019-08-25 11:20:06 -0400

    completed post details page w/ base styling

--- client/src/components/PostDetails.js
+++ client/src/components/PostDetails.js
@@ -1,20 +1,45 @@
 import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
 import { useQuery } from '@apollo/react-hooks';
 import { GET_POST } from '../queries/queries';
 
 const PostDetails = props => {
-  const [postData, setPostData] = useState([])
-  const { loading, error, data } = useQuery(GET_POST)
-  const { id } = props.match.params
-
-  useEffect(() => {
-    setPostData(data)
+  const { loading, error, data } = useQuery(GET_POST, {
+    variables: {
+      id:  props.match.params.id
+    }
   });
 
-  console.log(data)
+  if(loading){ return <h1>loading...</h1> };
+  if(error){ return <h1>an error has occured...</h1> }
+
   return(
-    <h1>Post Details</h1>
+    <Wrapper>
+      <Title>{ data.post.title }</Title>
+      <Content>{ data.post.content }</Content>
+    </Wrapper>
   );
 };
 
 export default PostDetails;
+
+const Wrapper = styled.div`
+  border: 1px solid black;
+  width: 60%;
+  margin: 3rem auto;
+  text-align: center;
+`
+
+const Title = styled.h1`
+  padding: 1rem 2rem;
+  color: whitesmoke;
+  font-size: 3rem;
+  width: 100%;
+  background-color: #333;
+`
+
+const Content = styled.p`
+  font-size: 2rem;
+  padding: 2rem 3rem;
+
+`
